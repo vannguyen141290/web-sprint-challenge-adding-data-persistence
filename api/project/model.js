039051104510152a43d07b5e272eedbeced89b60
thi <vannguyen141290@gmail.com>
2021-10-08 05:52:30 -0700

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,16 +1,34 @@
 const db = require('../../data/dbConfig')
 
-const getAll = () => {
-    return db('projects')
+const getAll = async () => {
+    const projects = await db('projects')
+    const result = projects.map(proj => {
+        if (proj.project_completed === 0) {
+            return ({
+                ...proj,
+                project_completed: false
+            })
+        } else {
+            return ({
+                ...proj,
+                project_completed: true
+            })
+        }
+    })
+    return result
 }
 
 const create = async project => {
     const id = await db('projects')
         .insert(project)
-    return db('projects')
+    const result = await db('projects')
         .select('projects.*')
         .where('project_id', id)
         .first()
+    return ({
+        ...result,
+        project_completed: result.project_completed === 0 ? false : true
+    })
 }
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,43 @@
 const db = require('../../data/dbConfig')
 
-const getAll = () => {
-    return db('tasks as t')
+const getAll = async () => {
+    let tasks = await db('tasks as t')
         .leftJoin('projects as p', 't.project_id', 'p.project_id')
-        .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+        .select(
+            't.task_id',
+            't.task_description',
+            't.task_notes',
+            't.task_completed',
+            'p.project_name',
+            'p.project_description'
+        )
+    const result = tasks.map(task => {
+        if (task.task_completed === 0) {
+            return ({
+                ...task,
+                task_completed: false
+            })
+        } else {
+            return ({
+                ...task, task_completed: true
+            })
+        }
+    })
+    return result
 }
 
 const create = async task => {
     const id = await db('tasks')
         .insert(task)
-    return db('tasks')
+    const newTask = await db('tasks')
+        .select('task_description', 'task_completed', 'task_notes')
         .where('task_id', id)
+        .first()
+    return ({
+        task_description: newTask.task_description,
+        task_completed: newTask.task_completed === 0 ? false : true,
+        task_notes: newTask.task_notes
+    })
 }
 
 module.exports = {
