b2d087be26ba4c76923b52e6e3f77fd72b43ed11
thi <vannguyen141290@gmail.com>
2021-10-08 04:21:46 -0700

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,13 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const getAll = () => {
+    return db('projects')
+}
+
+// - [ ] `[GET] /api/projects`
+//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+//     - Example of response body: `[{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]`
+
+module.exports = {
+    getAll
+}
