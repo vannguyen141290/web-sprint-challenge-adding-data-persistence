eeeb1d66aea28bfd52d651835d5ae20031504f76
thi <vannguyen141290@gmail.com>
2021-10-08 05:21:03 -0700

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,9 @@
 const db = require('../../data/dbConfig')
 
 const getAll = () => {
-    return db('tasks')
+    return db('tasks as t')
+        .leftJoin('projects as p', 't.project_id', 'p.project_id')
+        .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
 }
 
 const create = async task => {
